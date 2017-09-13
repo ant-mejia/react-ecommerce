8cefb4c8820450466badc46d9662bf9f2dad3a39
Anthony Mejia <git@antmejia.com>
2017-09-13 05:24:31 -0400

    Created helpers file # <- Subject message. Maximum 50 characters.   ->| # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

---
+++ server/helpers.js
@@ -0,0 +1,10 @@
+let helpers = {
+  generateUid: (length = 30) => {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < length; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
+}
+module.exports = helpers;
