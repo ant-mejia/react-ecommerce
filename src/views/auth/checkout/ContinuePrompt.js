47ff55b4e732a4f93194da426a19de9ee3e889e0
Anthony Mejia <git@antmejia.com>
2018-03-26 17:36:41 -0400

    Improved UI # <- Subject message. Maximum 50 characters.   ->| # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

--- src/views/auth/checkout/ContinuePrompt.js
+++ src/views/auth/checkout/ContinuePrompt.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import View from '../../../containers/View';
 
 class ContinuePrompt extends Component {
 
+  handleConfirm = () => {
+    this.props.respond(true)
+  }
+  handleReject = () => {
+    this.props.respond(false)
+  }
   render() {
     return (
-      <div>Prompt!</div>
+      <View>
+        <h1>Would you like to continue your previous order?</h1>
+        <button onClick={this.handleConfirm}>Yes</button>
+        <button onClick={this.handleReject}>No</button>
+      </View>
     );
   }
 
